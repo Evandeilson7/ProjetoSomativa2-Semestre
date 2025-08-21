86a5cec8e6cdc2ab84bf7b163aea271afad502bd
evandeilson silva sales <evandeilson2011@hotmail.com>
2025-08-20 22:47:17 -0300

    Desenvolvimento Front End/tailwindcss
    Desenvolvimento Front
    End

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p>© 2025 Agência de Viagem. Todos os direitos reservados.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer', // O nome do componente
+  props: {        // As propriedades esperadas pelo componente
+    store: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #eee;
+  padding: 1rem;
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>Agência de Viagem</h1>
+    <nav>
+      <a href="/">Início</a> |
+      <a href="/about">Sobre</a> |
+      <a href="/services">Serviços</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header', // O nome do componente
+  props: {        // As propriedades esperadas pelo componente
+    store: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  background-color: #eee;
+  padding: 1rem;
+  text-align: center;
+}
+nav a {
+  margin: 0 10px;
+  text-decoration: none;
+}
+</style>
